192e8a6a5d20377e598f8f8faa0e61d2600fb762
Iftekhar <akankha_haidar.outlook.com>
2023-11-21 19:12:05 +0600

    May be changed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const PORT = process.env.PORT || 5000
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.listen(5000, () => console.log('Server listening 5000'))
+app.listen(PORT, () => console.log(`Server listening ${PORT}`))
