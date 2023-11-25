6918ef38ea122bed4314b6db3eb751e03c74a50f
Iftekhar <akankha_haidar.outlook.com>
2023-11-25 09:12:38 +0600

    new file added

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+
 const logEvents = require('./logEvents')
-
 const EventEmitter = require('events')
-
-class MyEmitter extends EventEmitter {}
-
+class Emitter extends EventEmitter {}
 //initial object
+const myEmitter = new Emitter()
 
-const myEmitter = new MyEmitter()
+const PORT = process.env.PORT || 5000
 
-//add listener for the log event
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method)
 
-myEmitter.on('log', (msg) => logEvents(msg))
+  let link
+  if (req.url === '/' || req.url === 'index.html') {
+    res.statusCode = 200
+    res.setHeader('content-Type', 'text/html')
+    link = path.join(__dirname, 'views', 'index.html')
 
-setTimeout(() => {
-  //Emit event
-
-  myEmitter.emit('log', 'log event emiited')
+    fs.readFile(link, 'utf8', (err, data) => {
+      res.end(data)
+    })
+  }
 })
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// myEmitter.on('log', (msg) => logEvents(msg))
+
+// myEmitter.emit('log', 'log event emiited')
