5db2edb3277050f2c4a1ad630ccebd71ad825c5d
Iftekhar <akankha_haidar.outlook.com>
2023-11-28 12:22:33 +0600

    layout changed

--- routes/root.js
+++ routes/root.js
@@ -4,6 +4,7 @@ const path = require('path')
 router.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
+
 router.get('/new-page(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'))
 })
@@ -14,7 +15,7 @@ router.get('/old-page(.html)?', (req, res) => {
 router.all('*', (req, res) => {
   res.status(404)
   if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
+    res.sendFile(path.join(__dirname, '..', 'views', '404.html'))
   }
   if (req.accepts('json')) {
     res.json({ error: '404 Not found' })
